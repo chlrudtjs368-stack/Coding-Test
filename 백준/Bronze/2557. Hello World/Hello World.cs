8ae719ad8c8b44ff83af36d731f21f6b65500389
vvCapella <chlrudtjs368@gmail.com>
2025-12-16 15:27:17 +0900

    [Bronze V] Title: Hello World, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
