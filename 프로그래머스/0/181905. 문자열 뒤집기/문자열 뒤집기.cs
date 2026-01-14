353dc068e741e88fef93dece705bde548dd27486
vvCapella <chlrudtjs368@gmail.com>
2026-01-14 09:24:01 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.86 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int s, int e) {
+        
+        string left = my_string.Substring(0, s);
+        string mid = my_string.Substring(s, e - s + 1);
+        string right = my_string.Substring(e + 1);
+
+        char[] arr = mid.ToCharArray();
+        Array.Reverse(arr);
+        string reversedMid = new string(arr);
+        
+        return left + reversedMid + right;
+        
+    }
+}
