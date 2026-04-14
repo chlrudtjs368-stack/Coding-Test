70b06b7ad0156a5f831b2ccb895b7ab0b89da0cd
vvCapella <chlrudtjs368@gmail.com>
2026-04-14 18:03:56 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.20 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int slice, int n) 
+    {
+        int answer = n / slice;
+        
+        if (n % slice != 0)
+        {
+            answer++;
+        }
+        return answer;
+    }
+}
